2e7d0fd7d0ace1f28c263082209a519ff833a8af
Ashley Penney <apenney@edx.org>
2012-11-06 19:17:41 +0000

    Tweak this to be a define again.

--- manifests/server.pp
+++ manifests/server.pp
@@ -39,7 +39,7 @@ class mcollective::server(
     subscribe => File['/etc/mcollective/server.cfg'],
   }
 
-  class { 'mcollective::connector':
+  mcollective::connector { 'base':
     type => $connector_type,
     pool => [
       {
